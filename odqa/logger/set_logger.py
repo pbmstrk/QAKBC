8584a54a760d459c6efae41ed3a6b6465578159c
pbmstrk <pbmstrk@users.noreply.github.com>
2020-07-04 19:22:47 +0100

    bug fix logging

--- odqa/logger/set_logger.py
+++ odqa/logger/set_logger.py
@@ -5,16 +5,18 @@ def set_logger(log_path):
  
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
+    if (logger.hasHandlers()):
+        logger.handlers.clear()
+ 
 
-    if not logger.handlers:
-        # Logging to a file
-        file_handler = logging.FileHandler(log_path)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s :%(levelname)s : %(name)s: %(message)s'))
-        logger.addHandler(file_handler)
+    # Logging to a file
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s :%(levelname)s : %(name)s: %(message)s'))
+    logger.addHandler(file_handler)
 
-        # Logging to console
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(logging.Formatter('%(message)s'))
-        logger.addHandler(stream_handler)
+    # Logging to console
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter('%(asctime)s :%(levelname)s : %(name)s: %(message)s'))
+    logger.addHandler(stream_handler)
 
     return logger
