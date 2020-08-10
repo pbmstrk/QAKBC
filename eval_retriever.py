90a8e960942d9c9004453d3cde8eeca1447edb00
pbmstrk <pbmstrk@users.noreply.github.com>
2020-08-10 16:41:04 +0100

    updates

--- eval_retriever.py
+++ eval_retriever.py
@@ -1,91 +1,190 @@
+#!/usr/bin/env python3
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""Evaluate the accuracy of the DrQA retriever module."""
+
+import regex as re
+import logging
 import argparse
 import json
+import time
 import os
-import string
-import re
 
-from tqdm import tqdm
+from multiprocessing import Pool as ProcessPool
+from multiprocessing.util import Finalize
+from functools import partial
+from drqa import retriever, tokenizers
+from drqa.retriever import utils
 
-from odqa.logger import set_logger
-from odqa.retriever import TfidfDocRanker, BM25DocRanker, DocDB
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]',
+                        '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('dataset', type=str)
-parser.add_argument('ranker', type=str)
-parser.add_argument('retrieverpath', type=str)
-parser.add_argument('dbpath', type=str)
-parser.add_argument('--ndocs', type=int, default=30)
-parser.add_argument('--logfile', type=str, default='eval_retriever.log')
+# ------------------------------------------------------------------------------
+# Multiprocessing target functions.
+# ------------------------------------------------------------------------------
 
-def get_class(name):
-    if name == 'tfidf':
-        return TfidfDocRanker
-    if name == 'bm25':
-        return BM25DocRanker
+PROCESS_TOK = None
+PROCESS_DB = None
 
-def normalize_answer(s):
-    """Lower text and remove punctuation, articles and extra whitespace."""
-    def remove_articles(text):
-        return re.sub(r'\b(a|an|the)\b', ' ', text)
 
-    def white_space_fix(text):
-        return ' '.join(text.split())
+def init(tokenizer_class, tokenizer_opts, db_class, db_opts):
+    global PROCESS_TOK, PROCESS_DB
+    PROCESS_TOK = tokenizer_class(**tokenizer_opts)
+    Finalize(PROCESS_TOK, PROCESS_TOK.shutdown, exitpriority=100)
+    PROCESS_DB = db_class(**db_opts)
+    Finalize(PROCESS_DB, PROCESS_DB.close, exitpriority=100)
 
-    def remove_punc(text):
-        exclude = set(string.punctuation)
-        return ''.join(ch for ch in text if ch not in exclude)
 
-    def lower(text):
-        return text.lower()
+def regex_match(text, pattern):
+    """Test if a regex pattern is contained within a text."""
+    try:
+        pattern = re.compile(
+            pattern,
+            flags=re.IGNORECASE + re.UNICODE + re.MULTILINE,
+        )
+    except BaseException:
+        return False
+    return pattern.search(text) is not None
 
-    return white_space_fix(remove_articles(remove_punc(lower(s))))
 
-def initialise(args):
+def has_answer(answer, doc_id, match):
+    """Check if a document contains an answer string.
 
-    logger.info("Initialising retriever")
-    retriever = get_class(args.ranker)(args.retrieverpath)
-    db = DocDB(args.dbpath)
+    If `match` is string, token matching is done between the text and answer.
+    If `match` is regex, we search the whole text with the regex.
+    """
+    global PROCESS_DB, PROCESS_TOK
+    text = PROCESS_DB.get_doc_text(doc_id)
+    text = utils.normalize(text)
+    if match == 'string':
+        # Answer is a list of possible strings
+        text = PROCESS_TOK.tokenize(text).words(uncased=True)
+        for single_answer in answer:
+            single_answer = utils.normalize(single_answer)
+            single_answer = PROCESS_TOK.tokenize(single_answer)
+            single_answer = single_answer.words(uncased=True)
+            for i in range(0, len(text) - len(single_answer) + 1):
+                if single_answer == text[i: i + len(single_answer)]:
+                    return True
+    elif match == 'regex':
+        # Answer is a regex
+        single_answer = utils.normalize(answer[0])
+        if regex_match(text, single_answer):
+            return True
+    return False
 
-    return retriever, db
 
-def fetch_text(doc_id, db):
-    return db.get_doc_text(doc_id)
+def get_score(answer_doc, match):
+    """Search through all the top docs to see if they have the answer."""
+    answer, (doc_ids, doc_scores) = answer_doc
+    for doc_id in doc_ids:
+        if has_answer(answer, doc_id, match):
+            return 1
+    return 0
 
-if __name__ == '__main__':
 
-    # parse command line arguments
+# ------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------
+
+class EVAL_ARGS():
+    def __init__(self, dataset, model, doc_db, ranker, k1=1.2, b=0.75,
+                 tokenizer='simple', n_docs=5, num_workers=None, match='string'):
+        self.dataset = dataset
+        self.model = model
+        self.doc_db = doc_db
+        self.tokenizer = tokenizer
+        self.n_docs = n_docs
+        self.num_workers = num_workers
+        self.match = match
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset', type=str, default=None)
+    parser.add_argument('predictions', type=str, default=None)
+    parser.add_argument('--doc-db', type=str, default=None, help='Path to Document DB')
+
+    parser.add_argument('--tokenizer', type=str, default='simple')
+    parser.add_argument('--n-docs', type=int, default=5)
+    parser.add_argument('--num-workers', type=int, default=None)
+    parser.add_argument('--match', type=str, default='string', choices=['regex', 'string'])
     args = parser.parse_args()
+    return args
 
-    # set up logging
-    logger = set_logger(args.logfile)
 
-    # initialise retriever
-    retriever, db = initialise(args)
+def eval(args):
+    logger.info('Arguments: %s' % str(args))
+    # start time
+    start = time.time()
 
-    # open the dataset of queries and append to list
-    queries = []
+    # read all the data and store it
+    logger.info('Reading data ...')
+    questions = []
     answers = []
     for line in open(args.dataset):
         data = json.loads(line)
-        queries.append(data['question'])
-        answers.append(data['answer'])
+        question = data['question']
+        answer = data['answer']
+        questions.append(question)
+        answers.append(answer)
 
-    batches = [queries[i: i + 1000] for i in range(0, len(queries), 1000)]
-    j = 0
-    count = 0
-    total = 0
-    for i, batch in enumerate(batches):
-        logger.info('Retrieving results')
-        results = retriever.batch_closest_docs(batch, k=args.ndocs)
-        for result in tqdm(results):
-            doc_ids = result[0]
-            answer = answer[j]
-            total += 1
-            for doc_id in doc_ids:
-                text = normalize_answer(fetch_text(doc_id, db))
-                for ans in answer:
-                    if normalize_answer(ans) in text:
-                        count += 1
-                        break
-            j += 1
-        logger.info(str(count/total))
+    for line in open(args.predictions):
+        data = json.loads(line)
+        closest_docs = data['doc_ids']
+
+    answers_docs = zip(answers, closest_docs)
+
+    # define processes
+    tok_class = tokenizers.get_class(args.tokenizer)
+    tok_opts = {}
+    db_class = retriever.DocDB
+    db_opts = {'db_path': args.doc_db}
+    processes = ProcessPool(
+        processes=args.num_workers,
+        initializer=init,
+        initargs=(tok_class, tok_opts, db_class, db_opts)
+    )
+
+    # compute the scores for each pair, and print the statistics
+    logger.info('Retrieving and computing scores...')
+    get_score_partial = partial(get_score, match=args.match)
+    scores = processes.map(get_score_partial, answers_docs)
+
+    filename = os.path.basename(args.dataset)
+    stats = (
+        "\n" + "-" * 50 + "\n" +
+        "{filename}\n" +
+        "Examples:\t\t\t{total}\n" +
+        "Matches in top {k}:\t\t{m}\n" +
+        "Match % in top {k}:\t\t{p:2.2f}\n" +
+        "Total time:\t\t\t{t:2.4f} (s)\n"
+    ).format(
+        filename=filename,
+        total=len(scores),
+        k=args.n_docs,
+        m=sum(scores),
+        p=(sum(scores) / len(scores) * 100),
+        t=time.time() - start,
+    )
+    print(stats)
+
+    del ranker
+    processes.close()
+    processes.terminate()
+
+    result = (sum(scores) / len(scores) * 100)  # return the final metric
+    return result
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    eval(args)
